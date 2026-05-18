720b63296f29a5685dcc68082764d7c6fa2202bd
Victor Gabriel <victorrgabrielcs@gmail.com>
2026-05-18 13:41:38 -0300

    feat: add the default message to the email conflict error

--- backend/src/shared/errors/email.conflict.error.ts
+++ backend/src/shared/errors/email.conflict.error.ts
@@ -1,12 +1,12 @@
 import { ConflictError } from "./conflict.error";
 
 export class EmailConflictError extends ConflictError {
-  constructor({ message, cause }: EmailConflictOptions) {
+  constructor({ message = "Email already exists", cause }: EmailConflictOptions = {}) {
     super({ message, cause, code: "EMAIL_CONFLICT" });
   }
 }
 
 type EmailConflictOptions = {
-  message: string;
+  message?: string;
   cause?: unknown;
 };
