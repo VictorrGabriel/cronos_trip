c700bcac6437dc20c02faa944c0715b96d275087
Victor Gabriel <victorrgabrielcs@gmail.com>
2026-05-18 13:44:40 -0300

    feat: add the builder for the publicId in the helpers file

--- backend/src/shared/utils/index.ts
+++ backend/src/shared/utils/index.ts
@@ -1,10 +1,30 @@
 export { cleanByAllowedKeys } from "./object.helper";
 export { pickByKeys } from "./object.helper";
-export {toTimezoneMidnight, isValidProgressStatus} from "./validation.helper"
-export {isValidApiReference, isValidDailyQuota, isValidDayDate, isValidTotalEstimateCents} from "./itinerary.validation.helper";
-export {isValidVisitOrderNumber, hasMinuteLeft} from "./visitation.validation.helper";
-export {customNanoId, normalizeString, capitalizeFirstLetter} from "./helpers"
-export { generateAccessToken, generateRefreshToken, verifyAccessToken, verifyRefreshToken } from "./auth.helper";
-export { buildItineraryResponseDTO, buildTripResponseDTO, buildVisitationResponseDTO} from "./dto.response.builders";
-
-
+export { toTimezoneMidnight, isValidProgressStatus } from "./validation.helper";
+export {
+  isValidApiReference,
+  isValidDailyQuota,
+  isValidDayDate,
+  isValidTotalEstimateCents,
+} from "./itinerary.validation.helper";
+export {
+  isValidVisitOrderNumber,
+  hasMinuteLeft,
+} from "./visitation.validation.helper";
+export {
+  customNanoId,
+  normalizeString,
+  capitalizeFirstLetter,
+  buildPublicId
+} from "./helpers";
+export {
+  generateAccessToken,
+  generateRefreshToken,
+  verifyAccessToken,
+  verifyRefreshToken,
+} from "./auth.helper";
+export {
+  buildItineraryResponseDTO,
+  buildTripResponseDTO,
+  buildVisitationResponseDTO,
+} from "../dto/dto.response.builders";
