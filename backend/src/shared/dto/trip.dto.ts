54d13154fa0044678941f5ac1a5b65c8cd937bf0
Victor Gabriel <victorrgabrielcs@gmail.com>
2026-05-20 23:01:28 -0300

    refactor: standardize trip DTO naming

--- backend/src/shared/dto/trip.dto.ts
+++ backend/src/shared/dto/trip.dto.ts
@@ -1,8 +1,11 @@
 import type { TripCreateInput, TripUpdateInput } from "@modules/trips/schemas";
 import type { Trip } from "@prisma/client";
 
-export type CreateTripDTO = TripCreateInput & {userId: string};
+export type TripCreateDTO = TripCreateInput & { userId: string };
 
-export type UpdateTripDTO = TripUpdateInput;
+export type TripUpdateDTO = TripUpdateInput;
 
-export type ResponseTripDTO = Omit<Trip, "userId" | "id" | "publicId" > & {id: string, userId: string};
+export type TripResponseDTO = Omit<Trip, "userId" | "id" | "publicId"> & {
+  id: string;
+  userId: string;
+};
