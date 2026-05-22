fb743eb6ce4fb6264d04fdcc1f1452c27cd7b58e
Victor Gabriel <victorrgabrielcs@gmail.com>
2026-05-22 08:20:30 -0300

    refactor: move the validation files to the use cases within the modules.

---
+++ backend/src/modules/visitation/usecase/helpers.ts
@@ -0,0 +1,14 @@
+import { isInteger } from "@shared/utils";
+
+export const isValidVisitOrderNumber = (order: unknown): boolean => {
+  return (
+    typeof order === "number" && (isInteger(order) || order <= 10 || order > 0)
+  );
+};
+
+export const hasMinuteLeft = (minutesSum: number, minutesToAdd: number): boolean => {
+  const dayMinutes = 24 * 60;
+  const remainingMinutes = dayMinutes - minutesSum;
+
+  return remainingMinutes - minutesToAdd >= 0;
+};
---
+++ backend/src/shared/utils/index.ts
@@ -0,0 +1,15 @@
+export { cleanByAllowedKeys } from "./object.helper";
+export { pickByKeys } from "./object.helper";
+export { toTimezoneMidnight, isValidProgressStatus, isInteger } from "./validation.helper";
+export {
+  customNanoId,
+  normalizeString,
+  capitalizeFirstLetter,
+  buildPublicId
+} from "./global.helpers";
+export {
+  generateAccessToken,
+  generateRefreshToken,
+  verifyAccessToken,
+  verifyRefreshToken,
+} from "./auth.helper";
